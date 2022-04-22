09b9183f7cdf507dcfadaff98aaa282e6d8e1b8c
S-quark1 <binarakuanysheva13@gmail.com>
2022-04-22 16:58:14 +0600

    sample commit to make sure everything works

--- routes/accessories.js
+++ routes/accessories.js
@@ -4,7 +4,7 @@ const path = require('path');
 const https = require("https");
 router
     .route('/')
-    .get((req, res) => res.render(path.resolve('public/html_files/access.ejs'), {ans: 1, c: 'USD'}))
+    .get((req, res) => res.render(path.resolve('public/html_files/access.ejs'), {ans: 1, holder: 'USD'}))
     .post((req, res) => {
         let to = req.body.currency;
         let key = '39FBE301-1E2D-4502-9052-6BC86BB70AC8';
@@ -14,7 +14,7 @@ router
                 response.on('data', d => {
                     let data = JSON.parse(d);
                     let curr = data.rate;
-                    res.render(path.resolve('public/html_files/access.ejs'), {ans: curr, c: to});
+                    res.render(path.resolve('public/html_files/access.ejs'), {ans: curr, holder: to});
                 })
             });
         } catch (e) {
