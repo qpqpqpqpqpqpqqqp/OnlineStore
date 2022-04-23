362cc7114e400516d9b29eb28e1a63d9d5543d4f
S-quark1 <binarakuanysheva13@gmail.com>
2022-04-23 08:37:14 +0600

    asdf

--- routes/shoes.js
+++ routes/shoes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const path = require('path');
 const https = require("https");
+//something to do
+const port = 3000;
 router
     .route('/')
     .get((req, res) => res.render(path.resolve('public/html_files/shoe.ejs'), {ans: 1, holder: 'USD'}))
