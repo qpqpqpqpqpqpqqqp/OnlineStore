7d7d5f78c387ff5f0d8339545a1904e5d5a74134
S-quark1 <binarakuanysheva13@gmail.com>
2022-05-09 14:16:23 +0600

    heroku

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser")
 const app = express();
-const port = 3000;
 const mongoose = require('mongoose');
 const dbConfig = require('./config/database.config.js');
 app.use(bodyParser.urlencoded({extended: true}))
@@ -27,6 +26,11 @@ app.use('/cloth', require("./routes/clothes"))
 app.use('/shoes', require("./routes/shoes"))
 app.use('/accessories', require("./routes/accessories"))
 app.use('/profile', require("./routes/profile"))
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
 
 app.listen(port, () =>
     console.log(`App listening at http://localhost:${port}/home`)
